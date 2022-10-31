d2148a767d60cd90d45ece3be12a69c053c2b578
saulo-programador007 <108157787+saulo-programador007@users.noreply.github.com>
2022-10-31 10:10:54 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function newImage(){
+
+
+
+	fabric.Image.fromURL('birthdayImage.jpg', function(Img){
+
+       blockImageObject=Img
+       blockImageObject.scaleToWidth(700);
+       blockImageObject.scaleToHeight(510);
+       blockImageObject.set({
+        top:0,
+        left:0
+    })
+    canvas.add(blockImageObject);
+  })
+}
+
+
+
+
+function playSound() {
+    x.play();
+}
